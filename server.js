1ee012761ed1a105255ec095315a899e64e73b02
Sam Oaks <samuelclayoaks@yahoo.com>
2023-01-22 13:27:34 -0500

    added route code

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+const PORT = process.env.PORT || 3001;
 const express = require('express')
 const app = express()
+
 const path = require('path')
 const fs = require('fs')
-const notesRoute = require('./routes/notesRoute');
+const notesRoute = require('./routes/notesRoute/notesRoute');
 const htmlRoute = require('./routes/htmlRoute');
+
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/', notesRoute);
+
+app.use('/api', notesRoute);
 app.use('/', htmlRoute);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
